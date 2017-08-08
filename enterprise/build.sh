c8a1cfc0c33fb41ea6d5cb924026e7d46c6d5957
Joshua Hart <jahshuah@gmail.com>
2017-08-08 14:29:12 -0400

    Update build to 6.6.2

--- enterprise/build.sh
+++ enterprise/build.sh
@@ -3,5 +3,5 @@ if [ -z $CURRENT ]; then
 	CURRENT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 fi
 
-docker build --no-cache=true -t jahshuah/splunk:6.6.1 $CURRENT
+docker build --no-cache=true -t jahshuah/splunk:6.6.2 $CURRENT
 docker build -t jahshuah/splunk:latest $CURRENT
