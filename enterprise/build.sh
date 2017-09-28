75e4724545a0f28335a1fe9579bc3de8f2743149
Joshua Hart <jahshuah@gmail.com>
2017-09-28 08:20:53 -0400

    Updated for beta access

--- enterprise/build.sh
+++ enterprise/build.sh
@@ -3,5 +3,5 @@ if [ -z $CURRENT ]; then
 	CURRENT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 fi
 
-docker build --no-cache=true -t jahshuah/splunk:6.6.2 $CURRENT
-docker build -t jahshuah/splunk:latest $CURRENT
+#docker build --no-cache=true -t jahshuah/splunk:7.0.0 $CURRENT
+docker build -t jahshuah/splunk:beta $CURRENT
